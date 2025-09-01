8ef92067258a95534d27b519031f9bf58b97ec49
Pascal JT <pasjtene@yahoo.co.uk>
2025-09-01 19:26:25 +0100

    abouts

--- frontend/talodu/app/[lang]/shop/editshop/[id]/ProductAddComponent.tsx
+++ frontend/talodu/app/[lang]/shop/editshop/[id]/ProductAddComponent.tsx
@@ -10,7 +10,7 @@ interface ShopAddProps {
 }
 
 const ProductAddComponent = ({ shop, onSave, onCancel }: ShopAddProps) => {
-    const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8888';
+    //const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8888';
     const [shops, setShops] = useState<Shop[]>([]);
      const [product, setProduct] = useState<Product>({
             ID: 0,
